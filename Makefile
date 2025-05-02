dce174af3de376210bb476ea8e78b7f302d0f903
Socheat Sok <socheatsok78@gmail.com>
2025-05-02 15:28:06 +0700

    Initial commit

---
+++ Makefile
@@ -0,0 +1,4 @@
+it: build install
+
+build:
+	go build -o ${HOME}/.docker/cli-plugins/docker-stackx .
---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/docker/cli/cli-plugins/metadata"
+	"github.com/docker/cli/cli-plugins/plugin"
+	"github.com/docker/cli/cli/command"
+	"github.com/spf13/cobra"
+)
+
+var Version = "0.1.0"
+
+func main() {
+	if err := run(); err != nil {
+		_, _ = fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	cmd := &cobra.Command{
+		Use:   "stackx",
+		Short: "Docker Stack Extended",
+		Long:  "Extended Docker Stack CLI plugin",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return nil
+		},
+	}
+
+	cli, err := command.NewDockerCli()
+	if err != nil {
+		return fmt.Errorf("failed to create docker cli: %w", err)
+	}
+
+	return plugin.RunPlugin(cli, cmd, metadata.Metadata{
+		SchemaVersion: "0.1.0",
+		Vendor:        "Docker Inc.",
+		Version:       Version,
+	})
+}
